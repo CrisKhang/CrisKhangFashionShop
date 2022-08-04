4a70c5b65d0b8c5066f7e6d66fc6d6fed9a7f2bd
GiaKhangLab <khangtgps15054@gmail.com>
2022-08-04 14:23:22 +0700

    first commit

---
+++ PS15054_TranGiaKhang_Lab8/src/main/java/com/lab8/Ps15054TranGiaKhangLab8Application.java
@@ -0,0 +1,13 @@
+package com.lab8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ps15054TranGiaKhangLab8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ps15054TranGiaKhangLab8Application.class, args);
+	}
+
+}
